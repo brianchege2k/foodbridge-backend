87f7c4f7b3bae42cff9db1e5ff212c4c683ccf4b
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-08-08 09:51:17 +0300

    setting up the database

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///foodbridge.db'  # SQLite database URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable track modifications
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecret')  # Secret key for JWT
+    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecretkey')
