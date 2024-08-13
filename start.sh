d8f9e1c038eed0c352f83b9a6b16be2d9768a1ca
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-08-13 12:12:02 +0300

    Prepare project for deployment on Render

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gunicorn app:app --bind 0.0.0.0:$PORT
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the Flask application
+flask run --host=0.0.0.0 --port=$PORT
